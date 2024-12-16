c81692cab22ddb6e277cf5737471798e85bb9019
acostacos <acostacarlom@gmail.com>
2024-12-16 15:19:39 +0800

    Trained LeNet for other datasets

--- models/lenet.py
+++ models/lenet.py
@@ -3,21 +3,17 @@ from torch import nn
 
 class LeNet(nn.Module):
     """ Lenet-5 model. Taken from https://d2l.ai/chapter_convolutional-neural-networks/lenet.html """
-    def __init__(self, in_shape, out_shape, lr=0.1, device='cpu'):
+    def __init__(self, in_channels=1, out_channels=10, device='cpu'):
         super().__init__()
 
-        self.in_shape = in_shape
-        self.out_shape = out_shape
-        self.lr = lr
-
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2, device=device)
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=6, kernel_size=5, padding=2, device=device)
         self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, device=device)
         self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
         self.fc = nn.Sequential(
             nn.Linear(in_features=16*5*5, out_features=120, device=device), nn.Sigmoid(),
             nn.Linear(in_features=120, out_features=84, device=device), nn.Sigmoid(),
-            nn.Linear(in_features=84, out_features=self.out_shape, device=device),
+            nn.Linear(in_features=84, out_features=out_channels, device=device),
         )
     
     def forward(self, x):
